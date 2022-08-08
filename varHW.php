6fb3e355dce311d27c70c548cd7e774a747a7f0d
KoTuKKoKoCuK <110828666+KoTuKKoKoCuK@users.noreply.github.com>
2022-08-08 16:43:11 +0300

    Add files via upload

---
+++ varHW.php
@@ -0,0 +1,13 @@
+<?php
+$float_var = 3.14;
+$string_var = 'строка';
+$int_var = 365;
+$number_string_var = '3.14';
+
+// var_dump($float_var);
+// var_dump($string_var);
+// var_dump($int_var);
+// var_dump($number_string_var);
+
+echo $int_var;
+?>
