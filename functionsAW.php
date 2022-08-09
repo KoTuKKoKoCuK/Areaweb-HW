cf2a034df44f0707bb3ae7875a8e92aded223091
KoTuKKoKoCuK <110828666+KoTuKKoKoCuK@users.noreply.github.com>
2022-08-09 22:46:01 +0300

    Add files via upload

--- functionsAW.php
+++ functionsAW.php
@@ -3,13 +3,17 @@ $a = 90;
 $b = 73;
 $c = 32;
 
-function math($a, $b, $c){
-    if ($a > 0 && $b > 0 && $c > 0 && $b > $c) {
-    return $k = (100 * 109.24 * $a) / ($b - $c) ** 3;
+function math($a, $b, $c)
+{
+    if ($a > 0 && $b > 0 && $c > 0 && $b > $c)
+    {
+        return ceil((100 * 109.24 * $a) / ($b - $c) ** 3);
     }
-    else {
+    else
+    {
         return NULL;
     }
 }
-echo ceil(math($a, $b, $c));
+
+echo math($a, $b, $c);
 ?>
